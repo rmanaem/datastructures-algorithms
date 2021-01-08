ef4ba3382ff03bff505f74525f80418135e7522a
rmanaem <rmanaem@protonmail.ch>
2021-01-08 10:44:29 -0500

    Adding tail and head recursion in C++

---
+++ cpp/recursion/TailHead.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void tail(int n)
+{
+    if (n > 0)
+    {
+        std::cout << " " << n;
+        tail(n - 1);
+    }
+}
+
+void head(int n)
+{
+    if (n > 0)
+    {
+        head(n - 1);
+        std::cout << " " << n;
+    }
+}
+
+/* Add a breakpoint and debug
+to see how tail and head
+recursions work*/
+int main()
+{
+    int x = 7;
+
+    tail(x);
+    head(x);
+
+    return 0;
+}
