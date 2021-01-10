684c14df1c219a40654d3d7ae8fabc632d7e43d7
rmanaem <rmanaem@protonmail.ch>
2021-01-09 22:00:35 -0500

    Adding combination in C

---
+++ c/recursion/combination.c
@@ -0,0 +1,40 @@
+/* C program to illustrate 
+recursion using combination
+nCr and pascal's triangle */
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return fact(n - 1) * n;
+}
+
+int combination(int n, int r)
+{
+    int num, den;
+    num = fact(n);
+    den = fact(r) * fact(n - r);
+    return num / den;
+}
+
+int combination_pascal(int n, int r)
+{
+    if (r == 0 || n == r)
+    {
+        return 1;
+    }
+    return combination_pascal(n - 1, r - 1) + combination_pascal(n - 1, r);
+}
+
+int main()
+{
+    int x = 4, y = 3;
+
+    printf("%d\n", combination(x, y));
+    printf("%d\n", combination_pascal(x, y));
+
+    return 0;
+}
